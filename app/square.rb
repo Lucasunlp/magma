4e8801e6050807b75afb2b951171cc2ba3711166
Lucas Albo <luucasunlp@gmail.com>
2020-03-06 11:59:12 -0300

    world and square models edited

--- app/square.rb
+++ app/square.rb
@@ -1,9 +1,39 @@
 class Square
-  def initialize; end
+  def initialize(world, x_axis, y_axis)
+    @world = world
+    @x_axis = x_axis
+    @y_axis = y_axis
+    @live = false
+  end
 
-  def status; end
+  def dead?
+    !@live
+  end
 
-  def change_status; end
+  def dead!
+    @live = false
+  end
 
-  def around_square; end
+  def live?
+    @live
+  end
+
+  def live!
+    @live = true
+  end
+
+  def change_status
+    @live = !@live
+  end
+
+  def around_square
+    around_square = []
+    around_square.push(@world.square_at(self.x - 1, self.y - 1))
+    around_square.push(@world.square_at(self.x - 1, self.y))
+    around_square.push(@world.square_at(self.x - 1, self.y + 1))
+    around_square.push(@world.square_at(self.x + 1, self.y - 1))
+    around_square.push(@world.square_at(self.x + 1, self.y))
+    around_square.push(@world.square_at(self.x + 1, self.y + 1))
+    around_square
+  end
 end
--- app/world.rb
+++ app/world.rb
@@ -1,6 +1,22 @@
 require './app/square'
 class World
-  def initialize; end
+  def initialize(width, height)
+    @squares = []
+    # Creating matrix with my params
+    height.times do |x_axis|
+      @squares.push([])
+      width.times do |y_axis|
+        @squares[x_axis].push(Square.new(self, x_axis, y_axis))
+      end
+    end
+  end
 
-  def squares; end
+  def squares
+    # Returns a new array that is a one-dimensional flattening
+    @squares.flatten
+  end
+
+  def square_at(x_axis, y_axis)
+    @squares[x_axis][y_axis] if @squares[x_axis]
+  end
 end
