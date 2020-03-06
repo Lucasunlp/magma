177db0ad15d3889c6425d8d9bdd004fec8d11f3b
Lucas Albo <luucasunlp@gmail.com>
2020-03-06 10:49:22 -0300

    creating and initializing models

---
+++ app/square.rb
@@ -0,0 +1,9 @@
+class Square
+  def initialize; end
+
+  def status; end
+
+  def change_status; end
+
+  def around_square; end
+end
---
+++ app/world.rb
@@ -0,0 +1,6 @@
+require './app/square'
+class World
+  def initialize; end
+
+  def squares; end
+end
